13f68ddfa10e6865d1b6e85c000a4e66a0db71b6
Alejandra Delgado Reoyo <areoyo@gamma28.aulas.gsyc.urjc.es>
2016-09-19 10:35:56 +0200

    He añadido multiplicar y dividir

--- calc.py
+++ calc.py
@@ -12,6 +12,15 @@ def plus(op1, op2):
 def minus(op1, op2):
     """ Function to substract the operands """
     return op1 - op2
+    
+def div (op1, op2):
+    try:
+        return op1 / op2
+    except ZeroDivisionError:
+        sys.exit("Error: No se puede dividir entre 0")
+    
+def mult (op1, op2):
+    return op1 * op2
 
 if __name__ == "__main__":
     try:
@@ -24,6 +33,10 @@ if __name__ == "__main__":
         result = plus(operando1, operando2)
     elif sys.argv[2] == "resta":
         result = minus(operando1, operando2)
+    elif sys.argv[2] == "divide":
+        result = div (operando1, operando2)
+    elif sys.argv[2] == "multiplica":
+        result = mult (operando1, operando2)
     else:
         sys.exit('Operación sólo puede ser sumar o restar.')
 
