e652a99ea3351082b538fbdcd9f6126d38917ace
alejandra <a.delgadore17@gmail.com>
2016-09-19 19:44:23 +0200

    Resultado final del ejercicio 6

--- calcplus.py
+++ calcplus.py
@@ -5,25 +5,22 @@ import sys
 import calcoohija
 
 fich = open(sys.argv[1], 'r')
-
 lineas = fich.readlines()
 
 if __name__ == "__main__":
 
     calculadora = calcoohija.CalculadoraHija()
+    operadorDict = {'suma': calculadora.plus, 'resta': calculadora.minus,
+         'multiplica': calculadora.mult, 'divide': calculadora.div}
     
     for line in lineas:
         elementos = line[:-1].split(',') #LISTA: TROCEA POR EL CARACTER QUE LE INDICAMOS
         
-        operador = elementos[0]
-        operadorDict = {'suma': calculadora.plus, 'resta': calculadora.minus,
-         'multiplica': calculadora.mult, 'divide': calculadora.div}
-     
         operando1 = int(elementos[1])
         operando2 = int(elementos[2])
-        result = operadorDict[operador](operando1, operando2)
+        result = operadorDict[elementos[0]](operando1, operando2)
         
-        for numero in elementos [3:]:
-            result = operadorDict[operador](result, int(numero))
+        for numero in elementos[3:]:
+            result = operadorDict[elementos[0]](result, int(numero))
 
         print (result)
