a07f420cd4c1fc22fd431c1d8512515b2e444081
Alejandra Delgado Reoyo <areoyo@gamma28.aulas.gsyc.urjc.es>
2016-09-19 18:16:18 +0200

    Ejercicio con importacion ficheros y aplicacion de diccionarios

--- calcplus.py
+++ calcplus.py
@@ -4,7 +4,7 @@
 import sys
 import calcoohija
 
-fich = open('/tmp/git/datos', 'r')
+fich = open(sys.argv[1], 'r')
 
 lineas = fich.readlines()
 
@@ -12,31 +12,18 @@ if __name__ == "__main__":
 
     calculadora = calcoohija.CalculadoraHija()
     
-    for line in lineas[:-1]:
+    for line in lineas:
         elementos = line[:-1].split(',') #LISTA: TROCEA POR EL CARACTER QUE LE INDICAMOS
-        print (elementos[0])
         
+        operador = elementos[0]
+        operadorDict = {'suma': calculadora.plus, 'resta': calculadora.minus,
+         'multiplica': calculadora.mult, 'divide': calculadora.div}
+     
         operando1 = int(elementos[1])
         operando2 = int(elementos[2])
-        if elementos[0] == "suma":               
-            resultado = calculadora.plus(operando1, operando2)
-            for numero in elementos [3:]:
-                resultado = calculadora.plus(resultado, int(numero))
-        elif elementos[0] == "resta":               
-            resultado = calculadora.minus(operando1, operando2)
-            for numero in elementos [3:]:
-                resultado = calculadora.minus(resultado, int(numero))
+        result = operadorDict[operador](operando1, operando2)
+        
+        for numero in elementos [3:]:
+            result = operadorDict[operador](result, int(numero))
 
-        elif elementos[0] == "multiplica":               
-            resultado = calculadora.mult(operando1, operando2)
-            for numero in elementos [3:]:
-                resultado = calculadora.mult(resultado, int(numero))
-
-        elif elementos[0] == "divide":               
-            resultado = calculadora.div(operando1, operando2)
-            for numero in elementos [3:]:
-                resultado = calculadora.div(resultado, int(numero))
-        else:
-            sys.exit('Operación sólo puede ser sumar, restar, multiplicar  o dividir.')
-
-        print (resultado)
+        print (result)
