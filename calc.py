26cecd61c11d5cd26137b847910e06e573f55079
Alejandra Delgado Reoyo <areoyo@gamma28.aulas.gsyc.urjc.es>
2016-09-26 09:57:56 +0200

    Correccion con el programa de comprobacion check-p2.py

--- calc.py
+++ calc.py
@@ -3,22 +3,27 @@
 
 import sys
 
+
 def plus(op1, op2):
     """ Function to sum the operands """
     return op1 + op2
+    
 
 def minus(op1, op2):
     """ Function to substract the operands """
     return op1 - op2
+
     
 def div(op1, op2):
     try:
         return op1 / op2
     except ZeroDivisionError:
         sys.exit("Error: Division by zero is not allowed")
-    
+
+
 def mult(op1, op2):
     return op1 * op2
+
 
 if __name__ == "__main__":
     try:
@@ -26,10 +31,10 @@ if __name__ == "__main__":
         operando2 = int(sys.argv[3])
     except ValueError:
         sys.exit("Error: Non numerical parameters")
-    
+
     operador = sys.argv[2]
     operadorDict = {'suma': plus, 'resta': minus, 
        'multiplica': mult, 'divide': div}
-     
+
     result = operadorDict[operador](operando1, operando2)
     print(result)
